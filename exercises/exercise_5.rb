722c737c06e9f6800340e008daf2754ab5327ae8
emmarskillings <emmarskillings@gmail.com>
2018-07-17 16:32:01 +0000

    exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_stores = Store.count(:stores)
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = @total_revenue / @total_stores
+
+puts @total_revenue
+puts @average_revenue
+
+@highest_revenue = Store.where("annual_revenue >= ?", 1000000).count(:stores)
+
+puts @highest_revenue
